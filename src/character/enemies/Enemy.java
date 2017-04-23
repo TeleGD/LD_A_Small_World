f809abbe21328705108318b22f668b0ccb560178
canalarthur <canalarthur@yahoo.fr>
2017-04-23 14:39:30 +0200

    LoS conditions

--- src/character/enemies/Enemy.java
+++ src/character/enemies/Enemy.java
@@ -40,8 +40,22 @@ public class Enemy extends Entity{
 		return imgBattle;
 	}
 
-
-	
+	public Enemy(Level l,float x, float y){
+		imgBattle=new ArrayList<Image>();
+		try {
+			imgBattle.add(new Image("img/Char_Battle/ennemy1droitestop.png"));
+			imgBattle.add(new Image("img/Char_Battle/ennemy1droitePD.png"));
+			imgBattle.add(new Image("img/Char_Battle/ennemy1droitePG.png"));
+			imgBattle.add(new Image("img/Char_Battle/ennemy1gauchestop.png"));
+			imgBattle.add(new Image("img/Char_Battle/ennemy1gauchePD.png"));
+			imgBattle.add(new Image("img/Char_Battle/ennemy1gauchePG.png"));
+		} catch (SlickException e) {
+			System.out.println("Enemy images couldn't be loaded");
+		}
+		level = l;
+		this.x = x;
+		this.y = y;
+	}	
 	
 	public Enemy(Level l){
 		imgBattle=new ArrayList<Image>();
@@ -85,26 +99,85 @@ public class Enemy extends Entity{
 	public boolean isInView(Entity p){
 		//We check the cases on the line between the two entities.
 		boolean result = true;
-		ArrayList<Object> cases = new ArrayList<Object>();
+		float lineY,lineX,coeff;
+		int side;
 		//Main steps of the algorithm (note that the steps can be done in one pass.)
 		
 		//First step : getting the cases
 			//Each case within the rectangle formed by the two entities is a candidate
-			// if the line cross the case, it goes into our list.
 			// Line equation : lineY = (y-p.getY())*lineX/(x-p.getX()) + x 
 			// For each x covered by the case, we check lineY. 
-		for(Cell[] col : level.grid){
-			System.out.println("new line");
-			for(Cell c : col){
-				System.out.print("|cell");
-			}
-			System.out.println("|");
-		}
 		//Second step : check if they obstruct the line of sight.
 			// We also have to check which sides are in the way of the LoS : if there is a wall on one of those sides, we have to return false.
-			
 		//Third step : update the nearest case and return the result.
 			// If the LoS is obstructed, the nearest case is the current case in order to stay in place?
+		
+		if(x == p.getX()){//Vertically aligned
+			if(y <= p.getY()){//Going down
+				coeff = 1;
+			}else coeff = -1;//Going up
+			
+			for(Cell[] lin : level.grid){
+				for(Cell c : lin){
+					if(c.getX() >= x && c.getX() <= x+64 ){
+						if(coeff > 0){
+							//Current case
+							if(c.getY() < y && c.getY() + 64 > y){
+								if(c.getCellType() == 1 ||c.getCellType() == 3||c.getCellType() == 4||c.getCellType() == 5||c.getCellType() == 6||c.getCellType() == 8||c.getCellType() == 10) return false;
+							}
+							// Other cases
+							if(c.getY() + 64 < p.getY() && c.getY() > y){
+								if((c.getCellType() >=0 && c.getCellType() <= 8)|| c.getCellType() == 10 || c.getCellType() == 13) return false;
+							}
+							// Last (player) case
+							if(c.getY() < p.getY() && c.getY()+64 > p.getY()){
+								if(c.getCellType() == 0 ||c.getCellType() == 2||c.getCellType() == 5||c.getCellType() == 6||c.getCellType() == 7||c.getCellType() == 8||c.getCellType() == 13) return false;
+							}
+						}else {
+							// Current case
+							if(c.getY() < y && c.getY() + 64 > y){
+								if(c.getCellType() == 0 ||c.getCellType() == 2||c.getCellType() == 5||c.getCellType() == 6||c.getCellType() == 7||c.getCellType() == 8||c.getCellType() == 13) return false;
+							}
+							// Other cases
+							if(c.getY() > p.getY() && c.getY() < y){
+								if((c.getCellType() >=0 && c.getCellType() <= 8)|| c.getCellType() == 10 || c.getCellType() == 13) return false;
+							}
+							// Last (player) case
+							if(c.getY() < p.getY() && c.getY()+64 > p.getY()){
+								if(c.getCellType() == 1 ||c.getCellType() == 3||c.getCellType() == 4||c.getCellType() == 5||c.getCellType() == 6||c.getCellType() == 8||c.getCellType() == 10) return false;
+							}
+						}
+					}
+				}
+			}
+		}else{
+			coeff = (y-p.getY())/(x-p.getX());
+			for(Cell[] lin : level.grid){
+				System.out.println("new line");
+				for(Cell c : lin){
+					System.out.print("|cell");
+					for(lineX = c.getX(); lineX < c.getX()+64; lineX++){
+						lineY = lineX*coeff + y;
+						if(lineY>=c.getY() && lineY<=c.getY()+64) {
+							if(c.getX()*coeff+y < c.getY() || (c.getX()+64)*coeff+y < c.getY()){
+								//Up
+							}
+							if(c.getX()*coeff+y > c.getY()+64 || (c.getX()+64)*coeff+y > c.getY()+64){
+								//Down
+							}
+							if(c.getX()*coeff+y > c.getY() && c.getX()*coeff+y < c.getY()+64){
+								//Left
+							}
+							if((c.getX()+64)*coeff+y > c.getY() && (c.getX()+64)*coeff+y < c.getY()+64){
+								//Right
+							}
+						}
+					}
+				}
+				System.out.println("|");
+			}
+		}
+		
 		return result;
 	}
 	
